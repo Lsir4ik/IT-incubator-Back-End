05e0dc39b7325611e13afac2f01a1bbf25959ae9
Sergey L <lazarev171193@mail.ru>
2023-03-05 20:47:18 +0300

    homework 1 test

---
+++ Week1/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {videoRouter} from "./router/video-router";
+
+const app = express();
+const port = process.env.PORT || 5000;
+const parserMiddleWare = bodyParser();
+
+// Middleware
+app.use(parserMiddleWare); // -- bodyParser
+app.use(videoRouter); // -- Routing
+
+app.listen(port, () => {
+    console.log(`App started at ${port} port!`)
+})
